ed7dd043eb5f9f6be63e7fcf72ae5f96bf4f36f4
Sebastien <e19b592p@etu.univ-nantes.fr>
2020-06-02 10:44:34 +0200

    Ajout de tables

--- database.sql
+++ database.sql
@@ -1,37 +1,48 @@
-drop table if exists users;
-drop table if exists answers;
-drop table if exists scores;
+drop database if exists patientDatabase;
+create database patientDatabase;
+use patientDatabase;
 
 
  -- Create tables --
 
 create table users (
-    patientId int primary key,
+    patientId int(10) primary key,
     firstName varchar(50) not null,
     lastName varchar(50) not null,
-    age int not null,
+    age int(10) not null,
     parentFirstName varchar(50) not null,
     parentLastName varchar(50) not null,
     parentMail varchar(70) not null,
-    phoneNumber varchar(20) not null
+    phoneNumber varchar(20) not null,
+);
+
+create table authentication (
+    patientId int(10) primary key,
+    mail char(100) not null,
+    password char(100) not null,
+    type int(3) not null,
+    foreign key (patientId) references users(patientId)
 );
 
 create table answers (
-    patientId int primary key,
-    answer1 varchar(30) not null
+    patientId int(10) primary key,
+    answer1 varchar(30) not null,
+    foreign key (patientId) references users(patientId)
 );
 
 create table scores (
-    patientId int primary key,
-    score int not null
+    patientId int(10) primary key,
+    score int(10) not null,
+    foreign key (patientId) references users(patientId)
 );
 
 create table pictures (
-    patientId int primary key,
-    picture1Id int not null,
-    picture2Id int not null,
-    picture1Name int not null,
-    picture2Name int not null
+    patientId int(10) primary key,
+    picture1Id int(10) not null,
+    picture2Id int(10) not null,
+    picture1Name int(10) not null,
+    picture2Name int(10) not null,
+    foreign key (patientId) references users(patientId)
 );
 
 
